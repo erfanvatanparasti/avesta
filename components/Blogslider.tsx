9135bffbd51e2b3f96dd0ef377ce4168f350fecf
erfanvatanparasti <erfan.vatanparasti@gmail.com>
2026-02-04 06:28:21 +0330

    vendor & update

--- components/Blogslider.tsx
+++ components/Blogslider.tsx
@@ -16,7 +16,7 @@ export default function Blogslider(props: any){
     }).sort(blog => blog.order == "active"? 1: -1));
   };
   const [currentSlide, setCurrentSlide] = useState("01");
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [blogs, setBlogs] = useState<blogSliderType[]>([]);
   useEffect(() => {
     changeSlide(currentSlide);
@@ -27,71 +27,54 @@ export default function Blogslider(props: any){
       setLoading(true);
       new Promise((res, rej) => {
         setTimeout(() => {
+          const data: blogSliderType[] = [
+            {
+              id: "03",
+              title: "۲۶ آذر، روز ملی حمل‌ونقل ایران گرامی باد",
+              text: "حمل‌ونقل، شریان حیاتی اقتصاد و زیرساخت توسعه پایدار کشور است. از جاده و ریل تا دریا و هوا، این صنعت نقشی تعیین‌کننده در پایداری زنجیره تأمین، رشد تجارت و اتصال هوشمند کسب‌وکارها",
+              image: "blog-03.jpg",
+              order: 'unactive'
+            },
+            {
+              id: "02",
+              title: "۲۶ آذر، روز ملی حمل‌ونقل ایران گرامی باد",
+              text: "حمل‌ونقل، شریان حیاتی اقتصاد و زیرساخت توسعه پایدار کشور است. از جاده و ریل تا دریا و هوا، این صنعت نقشی تعیین‌کننده در پایداری زنجیره تأمین، رشد تجارت و اتصال هوشمند کسب‌وکارها",
+              image: "blog-02.jpg",
+              order: 'active'
+            },
+            {
+              id: "01",
+              title: "۲۶ آذر، روز ملی حمل‌ونقل ایران گرامی باد",
+              text: "حمل‌ونقل، شریان حیاتی اقتصاد و زیرساخت توسعه پایدار کشور است. از جاده و ریل تا دریا و هوا، این صنعت نقشی تعیین‌کننده در پایداری زنجیره تأمین، رشد تجارت و اتصال هوشمند کسب‌وکارها",
+              image: "blog-01.jpg",
+              order: 'unactive'
+            },
+          ]
+          setBlogs(data.sort(blog => blog.order == "active"? 1: -1))
           res(1);
-        },2000);
+          setLoading(false);
+        }, 2000);
       });
-      const data: blogSliderType[] = [
-        {
-          id: "03",
-          title: "۲۶ آذر، روز ملی حمل‌ونقل ایران گرامی باد",
-          text: "حمل‌ونقل، شریان حیاتی اقتصاد و زیرساخت توسعه پایدار کشور است. از جاده و ریل تا دریا و هوا، این صنعت نقشی تعیین‌کننده در پایداری زنجیره تأمین، رشد تجارت و اتصال هوشمند کسب‌وکارها",
-          image: "blog-03.jpg",
-          order: 'unactive'
-        },
-        {
-          id: "02",
-          title: "۲۶ آذر، روز ملی حمل‌ونقل ایران گرامی باد",
-          text: "حمل‌ونقل، شریان حیاتی اقتصاد و زیرساخت توسعه پایدار کشور است. از جاده و ریل تا دریا و هوا، این صنعت نقشی تعیین‌کننده در پایداری زنجیره تأمین، رشد تجارت و اتصال هوشمند کسب‌وکارها",
-          image: "blog-02.jpg",
-          order: 'active'
-        },
-        {
-          id: "01",
-          title: "۲۶ آذر، روز ملی حمل‌ونقل ایران گرامی باد",
-          text: "حمل‌ونقل، شریان حیاتی اقتصاد و زیرساخت توسعه پایدار کشور است. از جاده و ریل تا دریا و هوا، این صنعت نقشی تعیین‌کننده در پایداری زنجیره تأمین، رشد تجارت و اتصال هوشمند کسب‌وکارها",
-          image: "blog-01.jpg",
-          order: 'unactive'
-        },
-      ]
-      setBlogs(data.sort(blog => blog.order == "active"? 1: -1))
-      setLoading(false);
     }
     fetchBlogs();
   }, [])
   return (
-      <div className={`blog-slider ${props.className} my-5 mx-auto w-full max-w-3xl px-6 tablet:px-0 `}>
+      <div className={`${props.className} my-5 mx-auto w-full max-w-3xl px-6 tablet:px-0 `}>
         <div className="flex flex-col items-center justify-center max-w-4xl mx-auto text-primary dark:text-white">
           <h3 className="text-HeadingMx tablet:text-HeadingL font-bold py-5 text-center w-full">
               نگاهی به <span className="text-orange">رویداد </span>های مهم اوستا هوش‌یار
           </h3>
         </div>
-        <div className="mt-10 py-5 relative">
+        <div className="mt-10 py-5 relative blog-slider">
           {loading ? <>
-          <div className="blog-item blog-previous py-0 rounded-xl shadow-left-side">
-            <Image src={`/imgs/blogs/blog-02.jpg`} height={300} width={768} alt=""/>
-            <div className="blog-content shadow-left-side flex flex-col justify-center gap-y-10 px-5 bg-bg-primary dark:bg-[#501665] rounded-xl text-center">
-              <h4 className="text-HeadingM text-right mx-auto">۲۶ آذر، روز ملی حمل‌ونقل ایران گرامی باد</h4>
+          <div className="blog-item blog-loading h-[300px] py-0 rounded-xl shadow-left-side">
+            <div className="blog-content flex flex-col justify-center gap-y-10 px-5 bg-bg-primary dark:bg-[#501665] rounded-xl text-center">
+              <h4 className="text-HeadingM text-right mx-auto">در حال دریافت اطلاعات</h4>
               <hr className="h-[2px] bg-primary"/> 
-              <p className="text-Body">حمل‌ونقل، شریان حیاتی اقتصاد و زیرساخت توسعه پایدار کشور است. از جاده و ریل تا دریا و هوا، این صنعت نقشی تعیین‌کننده در پایداری زنجیره تأمین، رشد تجارت و اتصال هوشمند کسب‌وکارها</p>
-              <p className="w-auto border-primary dark:border-secondary border-2 py-2 rounded-full leading-6 flex items-center justify-center gap-x-3"><span>➡</span> مشاهده جزئیات خبر</p> 
-            </div>
-          </div>
-          <div className="blog-item blog-active py-3 z-10">
-            <Image src={`/imgs/blogs/blog-01.jpg`} height={300} width={768} alt=""/>
-            <div className="blog-content shadow-left-side flex flex-col justify-center gap-y-10 px-5 bg-bg-primary dark:bg-[#501665] rounded-xl text-center">
-              <h4 className="text-HeadingM text-right mx-auto">۲۶ آذر، روز ملی حمل‌ونقل ایران گرامی باد</h4>
-              <hr className="h-[2px] bg-primary"/> 
-              <p className="text-Body">حمل‌ونقل، شریان حیاتی اقتصاد و زیرساخت توسعه پایدار کشور است. از جاده و ریل تا دریا و هوا، این صنعت نقشی تعیین‌کننده در پایداری زنجیره تأمین، رشد تجارت و اتصال هوشمند کسب‌وکارها</p>
-              <Link href="/" className="w-auto border-primary dark:border-secondary border-2 py-2 rounded-full leading-6 flex items-center justify-center gap-x-3"><span>➡</span> مشاهده جزئیات خبر</Link> 
-            </div>
-          </div>
-          <div className="blog-item blog-next py-0 rounded-xl shadow-left-side">
-            <Image src={`/imgs/blogs/blog-02.jpg`} height={300} width={768} alt=""/>
-            <div className="blog-content shadow-left-side flex flex-col justify-center gap-y-10 px-5 bg-bg-primary dark:bg-[#501665] rounded-xl text-center">
-              <h4 className="text-HeadingM text-right mx-auto">۲۶ آذر، روز ملی حمل‌ونقل ایران گرامی باد</h4>
-              <hr className="h-[2px] bg-primary"/> 
-              <p className="text-Body">حمل‌ونقل، شریان حیاتی اقتصاد و زیرساخت توسعه پایدار کشور است. از جاده و ریل تا دریا و هوا، این صنعت نقشی تعیین‌کننده در پایداری زنجیره تأمین، رشد تجارت و اتصال هوشمند کسب‌وکارها</p>
-              <p className="w-auto border-primary dark:border-secondary border-2 py-2 rounded-full leading-6 flex items-center justify-center gap-x-3"><span>➡</span> مشاهده جزئیات خبر</p> 
+              <p className="text-Body">
+                در حال دریافت اخبار و مقالات <br/>
+                لطفا منتظر بمانید
+              </p>
             </div>
           </div>
           </>:
